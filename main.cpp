9cf9d9aa4a5e69b9eb1d10bea3f15188195fe564
Erwin <thehowlong@icloud.com>
2026-03-14 19:45:18 -0500

    redesign: implement validator

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 // define what a csv engine is and what it does
 // mmap does exactly that: it 'loads' the whole file into memory as a 1D array
-std::string line;
 
-// if you have void parse it means you can call it any number of times with the
-// same instance of CSVObject which makes zero sense
-class CSVObject {                 // return CSVDocument?
-  std::ifstream normalized_input; // reference to disk (read mmap and laziness)
-public:
-  void parse(std::ifstream raw) {
-    if (!raw.is_open()) {
-      std::cout << "File could not be opened" << std::endl;
-    };
+void is_valid_csv(std::ifstream &csv); // return Valid, Invalid
+void parse(std::ifstream &csv);        // return Success or HasUnresolved
 
-    while (std::getline(raw, line)) {
-      std::cout << line << std::endl;
-    }
+std::string PATH_TO_CSV = "./example.csv";
+int main() {
+  std::ifstream csv(PATH_TO_CSV);
+  is_valid_csv(csv);
+
+  return 0;
+}
+
+void is_valid_csv(std::ifstream &file) {
+  if (!file.is_open()) {
+    std::cout << "File could not be opened" << std::endl;
+  };
+  if ()
+    std::string line;
+
+  // file extension is not csv
+  //  file is not empty
+  //  check first line for binary content (null or non-text chars fail)
+  //  check first line parses as header
+  while (std::getline(file, line)) {
+    std::cout << line << std::endl;
   }
-};
+  // assert file is csv
+}
 
-// row members members, headers,rows behavior
-// init
-// contruct
-// read and normalize raw
-// write to disk
-// get setters
-
-int main() { return 0; }
+void parse(std::ifstream &file) {
+  // parse -> returns enum with resolved/unresolved writes, most common error
+  // code, time, etc
+  // parse hands off to resolved db or error db
+}
