3667ca9fd3653d0c05a128bc3eb53f09c0fd35c0
howlingfantods_ <howlingfantods-dev@users.noreply.github.com>
2026-03-21 19:09:05 -0500

    added csv parsing

--- is_csv_valid.hpp
+++ is_csv_valid.hpp
@@ -12,10 +12,9 @@ using ValidatorResult = std::variant<Valid, Invalid>;
 
 ValidatorResult is_csv_valid(std::string file); // return Valid, Invalid
 
-ValidatorResult is_csv_valid(std::string path, std::ifstream &reader) {
-  if (path.substr(path.size() - 4) != ".csv") {
-    return Invalid{"File extension not csv."};
-  };
+// number of values should be less than or equal to number of headers for a
+// given row if its greater its invalid
+ValidatorResult is_csv_valid(std::ifstream &reader) {
 
   if (!reader.is_open()) {
     return Invalid{"File cannot be opened."};
@@ -27,8 +26,7 @@ ValidatorResult is_csv_valid(std::string path, std::ifstream &reader) {
 
   std::array<char, 512> buffer;
   reader.read(buffer.data(), 512);
-
-  for (auto i = 0; i < static_cast<int>(reader.gcount()); ++i) {
+  for (std::streamsize i = 0; i < reader.gcount(); ++i) {
     // control chars 0 - 31 invalid, except \t tab, \n newline, \r carriage
     if (buffer[i] < 32 && buffer[i] != 9 && buffer[i] != 10 &&
         buffer[i] != 13) {
