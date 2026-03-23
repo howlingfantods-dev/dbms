d405fcb6614f152161b109a070e3a66e76628b3b
howlingfantods_ <howlingfantods-dev@users.noreply.github.com>
2026-03-22 19:40:11 -0500

    fixed windows carriage bug

--- is_csv_valid.hpp
+++ is_csv_valid.hpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <cstdio>
 #include <fstream>
 #include <iostream>
@@ -8,6 +7,8 @@
 struct Valid {};
 struct Invalid {
   std::string reason;
+  int error_row;
+  int error_col;
 };
 using ValidatorResult = std::variant<Valid, Invalid>;
 
@@ -17,43 +18,50 @@ ValidatorResult is_csv_valid(std::ifstream &reader) {
     return Invalid{"File is empty."};
   }
 
-  std::array<char, 512> buffer;
-  reader.read(buffer.data(), 512);
-  for (std::streamsize i = 0; i < reader.gcount(); ++i) {
-    // control chars 0 - 31 invalid, except \t tab, \n newline, \r carriage
-    if (buffer[i] < 32 && buffer[i] != 9 && buffer[i] != 10 &&
-        buffer[i] != 13) {
-      return Invalid{"Invalid characters found (likely not text file)."};
-    }
-  }
-
   reader.clear();
   reader.seekg(0);
   bool in_quote = false;
   bool field_start = true;
-
+  int row = 0;
+  int col = 0;
   while (!reader.eof()) {
     char curr = reader.get();
+    unsigned char currd = (unsigned char)curr;
+    if (currd < 32 && currd != 9 && currd != 10 && currd != 13) {
+      return Invalid{"Invalid characters found (likely not text file).\n", row,
+                     col};
+    }
+
     if (in_quote) {
+      char next = reader.peek();
       if (curr == '"') {
-        char next = reader.peek();
         if (next == '"') {
           reader.ignore();
-        } else if (next == '\n' || next == ',') {
+
+        } else if (next == '\n' || next == ',' || next == '\r') {
           in_quote = false;
         } else {
-          return Invalid{"Ambiguity found with quote due to double quotes"};
+          return Invalid{"Ambiguity found within quote due to double quotes\n",
+                         row, col};
         }
       }
     } else {
       if (curr == '"' && field_start) {
         in_quote = true;
       }
-      field_start = (curr == ',' || curr == '\n');
+      if (curr == ',') {
+        field_start = true;
+      } else if (curr == '\n') {
+        field_start = (curr == ',' || curr == '\n' || curr == '\r');
+        row++;
+        col = 0;
+      }
     }
+    col++;
   }
   if (in_quote) {
-    return Invalid{"Ambiguity found with quote due to double quotes"};
+    return Invalid{"Ambiguity found within quote due to double quotes\n", row,
+                   col};
   }
   reader.clear();
   reader.seekg(0);
