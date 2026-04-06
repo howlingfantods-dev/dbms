bc7543630f2379c715f9e011790a0eaded17ae91
howlingfantods_ <howlingfantods-dev@users.noreply.github.com>
2026-04-05 20:18:20 -0500

    separated types

---
+++ parse.cpp
@@ -0,0 +1,148 @@
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <variant>
+#include <vector>
+
+struct Valid {};
+
+struct Invalid {
+  std::string reason;
+  int error_row;
+  int error_col;
+};
+using ValidatorResult = std::variant<Valid, Invalid>;
+
+ValidatorResult is_csv_valid(std::ifstream &reader) {
+
+  if (reader.peek() == EOF) {
+    return Invalid{"File is empty."};
+  }
+
+  reader.clear();
+  reader.seekg(0);
+  bool in_quote = false;
+  bool field_start = true;
+  int row = 0;
+  int col = 0;
+  while (!reader.eof()) {
+    char curr = reader.get();
+    unsigned char currd = (unsigned char)curr;
+    if (currd < 32 && currd != 9 && currd != 10 && currd != 13) {
+      return Invalid{"Invalid characters found (likely not text file).\n", row,
+                     col};
+    }
+
+    if (in_quote) {
+      char next = reader.peek();
+      if (curr == '"') {
+        if (next == '"') {
+          reader.ignore();
+
+        } else if (next == '\n' || next == ',' || next == '\r') {
+          in_quote = false;
+        } else {
+          return Invalid{"Ambiguity found within quote due to double quotes\n",
+                         row, col};
+        }
+      }
+    } else {
+      if (curr == '"' && field_start) {
+        in_quote = true;
+      }
+      if (curr == ',') {
+        field_start = true;
+      } else if (curr == '\n') {
+        field_start = (curr == ',' || curr == '\n' || curr == '\r');
+        row++;
+        col = 0;
+      }
+    }
+    col++;
+  }
+  if (in_quote) {
+    return Invalid{"Ambiguity found within quote due to double quotes\n", row,
+                   col};
+  }
+  reader.clear();
+  reader.seekg(0);
+  return Valid{};
+}
+
+enum State {
+  Success,
+  PartialSuccess,
+  Failure,
+};
+
+struct ParseResult {
+  State state;
+  std::vector<std::string> invalid_rows;
+  int error_code; // need some mapping code->error message
+};
+
+ParseResult parse(std::ifstream &csv); // return Success or HasUnresolved
+
+// push to db if row is valid,
+ParseResult parse(std::ifstream &reader) {
+  int success_count;
+  int failure_count;
+  int runtime;
+  std::cout << "Initiating parsing" << std::endl;
+  bool IN_VALUE = false;
+  std::vector<std::string> row;
+  uint64_t count = 0;
+  std::stringstream value;
+  while (!reader.eof()) {
+    char curr = reader.get();
+    if (curr == '"') {
+      if (!IN_VALUE) {
+        if (reader.tellg() == 0) {
+          printf("%d\n", (int)reader.tellg());
+          IN_VALUE = true;
+        } else {
+          value << curr;
+        }
+        continue;
+      }
+      char next = reader.peek();
+      if (next == '\n' || next == ',') {
+        IN_VALUE = false;
+      } else if (next == '"') {
+        value << curr;
+        reader.ignore();
+      } else {
+        return ParseResult{
+            Failure,
+            {},
+            1,
+        };
+      }
+    } else if (curr == ',' && !IN_VALUE) {
+      row.push_back(value.str());
+      value = std::stringstream();
+    } else if (curr == '\n' && !IN_VALUE) {
+      row.push_back(value.str());
+      value = std::stringstream();
+      if (count == 0) {
+        Schema create_schema(std::vector<std::string> * row);
+        // schema == array of columns(we know the length)
+        // columns and their types can be defined from first row
+      } else {
+        serialize(std::vector<std::string> * row);
+      }
+      for (auto &i : row) {
+        std::cout << i + "|";
+      }
+      std::cout << std::endl;
+      // write to page
+      row.clear();
+    } else {
+      value << curr;
+    }
+  }
+  return ParseResult{Success, {}, -1};
+}
